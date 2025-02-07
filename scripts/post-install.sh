c5036428df025fa9ca9ac80563574d6195e7ed66
idkingz <idkingz.one@gmail.com>
2025-02-07 14:55:26 -0300

    feat: add greeting

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,1 +1,13 @@
-echo "hi!"
+
+echo ",---,              |"
+echo " .-' ,---.,---.,---|,---.,---."
+echo "|    |   ||    |   ||   ||   | says Hi!"
+echo " ---''---''    '---''---''   '"
+
+echo "Zordon is a minimal Ubuntu 24.04+ installations only!"
+echo "Begin installation!"
+
+#apt-get update >/dev/null
+#apt-get install -y git >/dev/null
+
+echo "Installation finishing..."
