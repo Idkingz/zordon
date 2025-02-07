f3a760e9aa39d99eec0a125dad41277c12c8ac50
idkingz <idkingz.one@gmail.com>
2025-02-07 16:11:48 -0300

    feat: checkout repo to local machine

--- scripts/post-install.sh
+++ scripts/post-install.sh
@@ -1,4 +1,3 @@
-
 echo ",---,              |"
 echo " .-' ,---.,---.,---|,---.,---."
 echo "|    |   ||    |   ||   ||   | says Hi!"
@@ -7,7 +6,14 @@ echo " ---''---''    '---''---''   '"
 echo "Zordon is a minimal Ubuntu 24.04+ installations only!"
 echo "Begin installation!"
 
-#apt-get update >/dev/null
-#apt-get install -y git >/dev/null
+apt-get update >/dev/null
+apt-get install curl -y >/dev/null
+apt-get install -y git >/dev/null
+
+git clone https://github.com/Idkingz/zordon.git ~/.local/share/zordon >/dev/null
+cd ~/.local/share/zordon
+latest_tag=$(git ls-remote --tags --sort=-v:refname https://github.com/Idkingz/zordon.git | head -n 1 | awk -F'/' '{print $3}')
+git clone --branch $latest_tag https://github.com/Idkingz/zordon.git
+
 
 echo "Installation finishing..."
